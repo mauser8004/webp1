d28f670d1e5be8bcb19085b737e5a3f7de649b64
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-02-24 13:41:04 +0100

    index header footer terv fooldal 2

--- index.php
+++ index.php
@@ -17,20 +17,8 @@
 	</header>
 	<?php include 'templates/index.tpl.php';?>
 	<footer>
-	<div id="googleMap" style="width:100%;height:400px;"></div>
-
-	<script>
-	function myMap() {
-	var mapProp= {
-	  center:new google.maps.LatLng(51.508742,-0.120850),
-	  zoom:5,
-	};
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	}
-	</script>
-
-	<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
-	<footer>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3060.083847739048!2d19.66604207669529!3d46.89513427113335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4743da7a6c479e1d%3A0xc8292b3f6dc69e7f!2sNeumann%20J%C3%A1nos%20Egyetem%20GAMF%20M%C5%B1szaki%20%C3%A9s%20Informatikai%20Kar!5e1!3m2!1shu!2shu!4v1740400801110!5m2!1shu!2shu" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	</footer>
 
 </body>
 </html>

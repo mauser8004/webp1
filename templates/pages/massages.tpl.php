ce89040e1796f23fdb8c19f1c3e674871270a295
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-02-24 19:11:26 +0100

    massages html teszt

---
+++ templates/pages/massages.tpl.php
@@ -0,0 +1,14 @@
+<div class=messages>
+ <table>
+  <tr>
+    <th>Mikor</th>
+    <th>Küldő</th>
+    <th>Üzenet</th>
+  </tr>
+  <tr>
+    <td>2025-02-24 19:05</td>
+    <td>Tobias</td>
+    <td>ide jönnek az üzenetek</td>
+  </tr>
+</table> 
+<div>

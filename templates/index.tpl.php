e7eb9315d512df1760a271c3c87bc822d6535beb
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-02-27 13:26:27 +0100

    index

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -2,9 +2,10 @@
 <p>
 
 Az Arduino egy szabad szoftveres, nyílt forráskódú elektronikai fejlesztőplatform, arra tervezve, hogy a különböző projektekben az elektronikus eszközök könnyebben hozzáférhetőek, kezelhetőek legyenek. Széles tömegek számára elérhető, mivel olcsó, könnyen beszerezhető, egyszerűen programozható, és csatlakoztatható más eszközökhöz.
-
+<br>
 A fejlesztői platform az úgynevezett IDE-ből, és egy Arduino Board-ból áll. Előbbi segítségével programokat írhatunk és tesztelhetünk számítógépen, utóbbi pedig egy hardver eszköz, amelyre az előzőleg elkészített programokat feltölthetjük a számítógépen keresztül, majd elektronikus eszközöket vezérelhetünk a segítségével.
 
+<br>
 Az Arduino Board elérhető kereskedelmi forgalomban kapható, előre összeszerelt, vagy otthon összeszerelhető alkatrészcsomagként. Mivel nyílt forráskódú a hardver is, bárki készíthet magának saját változatot belőle, vagy az eredetivel kompatibilis klónt.[1]
 Szoftver
 		 <iframe width="320" height="240" style="float: right;"
@@ -12,37 +13,37 @@ Szoftver
 		</iframe> 
 Az Arduino IDE egy kereszt-platformos Java nyelven írt fejlesztőkörnyezet, amely segítségével Arduino programokat készíthetünk, tesztelhetünk, majd az Arduino Board-okra tölthetjük. Az induláshoz a fejlesztőeszköz több mintaprogramot tartalmaz, amelyek segítségével egyszerűen megtanulhatjuk, hogyan tudunk LED-et villogtatni, fényerőt szabályozni, vagy a számítógéppel kommunikálni USB-n keresztül. Az Arduino programokat egy C/C++ alapú programozási nyelven írhatjuk, így az ezeket a nyelveket ismerők könnyen kiismerhetik magukat a környezetben. AZ IDE-hez tartozik a "Wiring" nevezetű C/C++ programkönyvtár, amellyel egyszerűen végezhetjük el a leggyakoribb input/output műveleteket.
 Hardver
-Board-ok</p>
+Board-ok</p><br>
 <p>
 Az Arduino Board-ok többféle változatban készülnek, amelyek méretben, a mikrovezérlő típusában, a belső memóriában, a be- és kimenetek számában különböznek. Vannak amelyek rendelkeznek beépített Ethernet, Bluetooth, Wi-Fi csatlakozási lehetőséggel.
-Forgalomban lévő modellek
+Forgalomban lévő modellek<br>
 
 
-    Arduino/Genuino UNO
+    Arduino/Genuino UNO<br>
 
-Széles körben használt modell, 8 bites, 16 MHz-es ATmega328P processzorral. 14 digitális I/O lábbal és 32 kB flash memóriával rendelkezik.[2]
+Széles körben használt modell, 8 bites, 16 MHz-es ATmega328P processzorral. 14 digitális I/O lábbal és 32 kB flash memóriával rendelkezik.[2]<br>
 
-    Arduino/Genuino 101
+    Arduino/Genuino 101<br>
 
-Egy kétmagos, kis fogyasztású, 32 bites, 32 MHz-es Intel Curie processzorral, Bluetooth LE támogatással rendelkező lap, ugyanazon perifériákkal, mint az UNO. Flash memóriája 192 kB méretű.[3]
+Egy kétmagos, kis fogyasztású, 32 bites, 32 MHz-es Intel Curie processzorral, Bluetooth LE támogatással rendelkező lap, ugyanazon perifériákkal, mint az UNO. Flash memóriája 192 kB méretű.[3]<br>
 
-    Arduino/Genuino Micro
+    Arduino/Genuino Micro<br>
 
-Kisméretű, ATmega32U4 processzorral rendelkező változat, amely micro USB csatlakozóval rendelkezik és breadboardon való használatra tervezték. Beépített USB vezérlője van, aminek segítségével akár egérként vagy billentyűként is működhet. 32 kB flash memóriája van.[4]
+Kisméretű, ATmega32U4 processzorral rendelkező változat, amely micro USB csatlakozóval rendelkezik és breadboardon való használatra tervezték. Beépített USB vezérlője van, aminek segítségével akár egérként vagy billentyűként is működhet. 32 kB flash memóriája van.[4]<br>
 
-    Arduino/Genuino Mega 2560
+    Arduino/Genuino Mega 2560<br>
 
-Összetettebb projekthez használható, 54 digitális I/O és 16 analóg bemeneti lábbal. ATmega2560 processzor és 256 kB flash memória van benne.[5]
+Összetettebb projekthez használható, 54 digitális I/O és 16 analóg bemeneti lábbal. ATmega2560 processzor és 256 kB flash memória van benne.[5]<br>
 
-    Arduino/Genuino Zero
+    Arduino/Genuino Zero<br>
 
-32 bites, nagyobb számítási teljesítményű változata az UNO-nak. Atmel SAMD21 processzor és 256 kB flash memória van benne. Előnye, hogy beépített debuggere is van.[6]
+32 bites, nagyobb számítási teljesítményű változata az UNO-nak. Atmel SAMD21 processzor és 256 kB flash memória van benne. Előnye, hogy beépített debuggere is van.[6]<br>
 
-    Arduino/Genuino MKR1000
+    Arduino/Genuino MKR1000<br>
 
 Wi-Fi támogatással ellátott, 32 bites lap, ami IoT projektekhez lett tervezve. Gyárilag működik akkumulátorról, tölti is azt USB-ről. SAMD21 és 256 kB flash memória van benne, valamint egy Wi-Fi modul és ahhoz való antenna.[7]
 </p>
-<p>Már nem gyártott modellek
+<p>Már nem gyártott modellek<br>
 
     Arduino Duemilanove
     Arduino Ethernet
@@ -55,27 +56,29 @@ Wi-Fi támogatással ellátott, 32 bites lap, ami IoT projektekhez lett tervezve
     Arduino Leonardo
     Arduino Esplora
     Arduino Yún
+</p>
+<p>
+Shield-ek<br>
+
+Az Arduino-k funkcionalitását könnyedén kiegészíthetjük az úgynevezett Shield-ekkel, amelyek a Board-okhoz egyszerűen illeszthető elektronikai áramkörök. Ezek segítségével azt Arduino-t akár közvetlenül az internetre csatlakoztathatjuk, motorokat, LED-eket, vezérelhetünk vele, vagy Wi-Fi, Bluetooth hálózatra kapcsolódhatunk. A jelenlegi hivatalos Arduino Shield-ek:
+<br>
+    Arduino WiFi Shield 101<br>
+    Arduino Proto Shield<br>
+    Arduino MKR Proto Shield<br>
+    Arduino MKR Proto Large Shield<br>
 	 	<video width="320" height="240" style="float: right" controls>
   		<source src="movie.mp4" type="video/mp4">
   		<source src="movie.ogg" type="video/ogg">
 			Your browser does not support the video tag.
 		</video>
-</p>
-<p>
-Shield-ek
 
-Az Arduino-k funkcionalitását könnyedén kiegészíthetjük az úgynevezett Shield-ekkel, amelyek a Board-okhoz egyszerűen illeszthető elektronikai áramkörök. Ezek segítségével azt Arduino-t akár közvetlenül az internetre csatlakoztathatjuk, motorokat, LED-eket, vezérelhetünk vele, vagy Wi-Fi, Bluetooth hálózatra kapcsolódhatunk. A jelenlegi hivatalos Arduino Shield-ek:
+    Arduino Yún Shield<br>
+    Arduino Motor Shield<br>
+    Arduino Ethernet Shield<br>
+    Arduino GSM Shield<br>
+    Arduino GPS Shield<br>
+    Arduino Servo Shield<br>
 
-    Arduino WiFi Shield 101
-    Arduino Proto Shield
-    Arduino MKR Proto Shield
-    Arduino MKR Proto Large Shield
-    Arduino Yún Shield
-    Arduino Motor Shield
-    Arduino Ethernet Shield
-    Arduino GSM Shield
-    Arduino GPS Shield
-    Arduino Servo Shield
 
 Ezeken kívül több cég gyárt világszerte Arduino kompatibilis Shield-eket, különböző célokra. 
 </p><br>

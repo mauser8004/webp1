73ae0ffe6157d3161264cb55f3352deabdc0ebb6
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-05 06:27:53 +0100

    array oldalak

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -1,5 +1,5 @@
 <header>
-
+<?php include("includes/config.inc.php");?>
 <div class="topnav" id="myTopnav">
   <a href="/" class="active">Főoldal</a>
   <a href="kepek">Képek</a>
@@ -10,7 +10,14 @@
     <i class="fa fa-bars"></i>
   </a>
 </div>
-
+					<?php foreach ($oldalak as $url => $oldal) { ?>
+						<?php if(! isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
+							<li<?= (($oldal == $keres) ? ' class="active"' : '') ?>>
+							<a href="<?= ($url == '/') ? '.' : $url ?>">
+							<?= $oldal['szoveg'] ?></a>
+							</li>
+						<?php } ?>
+					<?php } ?>
 
 <script>
 function myFunction() {

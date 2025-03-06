9ddff0f6cdfeadb148463b27fc6ecccffa7346a7
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-06 09:22:29 +0100

    array oldalak

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -1,24 +1,19 @@
 <header>
 <?php include("includes/config.inc.php");?>
 <div class="topnav" id="myTopnav">
-  <a href="/" class="active">Főoldal</a>
-  <a href="kepek">Képek</a>
-  <a href="kapcsolat">Kapcsolat</a>
-  <a href="uzenetek">Üzenetek</a>
-  <a href="login">Bejelentkezés</a>
+					<?php foreach ($oldalak as $url => $oldal) { ?>
+						<?php if(! isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
+							<?= (($oldal == $keres) ? ' class="active"' : '') ?>
+							<a href="<?= ($url == '/') ? '.' : $url ?>">
+							<?= $oldal['szoveg'] ?></a>
+							
+						<?php } ?>
+					<?php } ?>
+
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
 </div>
-					<?php foreach ($oldalak as $url => $oldal) { ?>
-						<?php if(! isset($_SESSION['login']) && $oldal['menun'][0] || isset($_SESSION['login']) && $oldal['menun'][1]) { ?>
-							<li<?= (($oldal == $keres) ? ' class="active"' : '') ?>>
-							<a href="<?= ($url == '/') ? '.' : $url ?>">
-							<?= $oldal['szoveg'] ?></a>
-							</li>
-						<?php } ?>
-					<?php } ?>
-
 <script>
 function myFunction() {
   var x = document.getElementById("myTopnav");

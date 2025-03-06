487b4cac748493be238983480aed448431c03c69
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-06 09:28:13 +0100

    array oldalak login jav

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -4,7 +4,7 @@ $oldalak = array(
 	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
 	'kepek' => array('fajl' => 'kepek', 'szoveg' => 'Képek', 'menun' => array(1,1)),
     'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(1,1)),
-    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'login' => array('fajl' => 'login', 'szoveg' => 'Belépés', 'menun' => array(1,1)),
     'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
     'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
     'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))

50a50a235e96e36dda9880d1cce4e5d364f44291
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-24 19:38:51 +0100

    ujra kezdes

---
+++ includes/config.inc.php
@@ -0,0 +1,29 @@
+<?php
+$ablakcim = array(
+    'cim' => 'WebProgramozás I. Beadandó',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo.png',
+    'kepalt' => 'logo',
+    'cim' => 'WebProgramozás I. Beadandó',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Tanulas Kft.'
+);
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+
+$oldalak = array(
+	'/' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+	'kepek' => array('fajl' => 'gallery', 'szoveg' => 'Képek', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'conntact', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+    'uzenetek' => array('fajl' => 'messages', 'szoveg' => 'Üzenetek', 'menun' => array(1,1)),
+    'login' => array('fajl' => 'login', 'szoveg' => 'Belépés', 'menun' => array(1,1)),
+    'kilepes' => array('fajl' => 'login', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'login', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('login' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+?>

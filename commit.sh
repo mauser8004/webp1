b43baa116d7350379c3586a6ac3d4577939805b0
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-24 20:32:03 +0100

    ujrakezdes5

---
+++ commit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git add *
+git commit -m $1
+git push origin main
+cp -r /home/zoli/prog3/webIbeadando/webp1local/webp1/* ~/prog3/webIbeadando/webp1ftp/
+
--- templates/pages/login.tpl.php
+++ templates/pages/login.tpl.php
@@ -1,1 +1,23 @@
-login
+    <form action = "belep" method = "post">
+      <fieldset>
+        <legend>Bejelentkezés</legend>
+        <br>
+        <input type="text" name="lname" placeholder="Felhasználó" required><br><br>
+        <input type="password" name="passwd" placeholder="Jelszó" required><br><br>
+        <input type="submit" name="belepes" value="Belépés">
+        <br>&nbsp;
+      </fieldset>
+    </form>
+    <h3>Regisztrálja magát, ha még nem felhasználó!</h2>
+    <form action = "regisztral" method = "post">
+      <fieldset>
+        <legend>Regisztráció</legend>
+        <br>
+        <input type="text" name="csaladinev" placeholder="Vezetéknév" required><br><br>
+        <input type="text" name="utonev" placeholder="Utónév" required><br><br>
+        <input type="text" name="lname" placeholder="Felhasználói név" required><br><br>
+        <input type="password" name="passwd" placeholder="Jelszó" required><br><br>
+        <input type="submit" name="regisztracio" value="Regisztráció">
+        <br>&nbsp;
+      </fieldset>
+    </form>

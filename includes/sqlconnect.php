f4761bdd2e2b5e4f1c2691f6b203a3f21da51aa8
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-25 18:23:13 +0100

    belepesteszt10

--- includes/sqlconnect.php
+++ includes/sqlconnect.php
@@ -3,6 +3,9 @@
         // Kapcsolódás
 	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 	$mysqli = new mysqli("localhost", "webp1db", "J3grvN7YjfVtBGwD2RxzdS", "webp1db");
+	if(mysqli_connect_errno()){
+ 				echo mysqli_connect_error();
+		}
 	$result = $mysqli->query("SELECT * FROM users", MYSQLI_USE_RESULT);
     }
     catch (PDOException $e) {

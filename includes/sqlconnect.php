4c52063fdd520b55f89bc1b7d57608f4d0efda0e
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-25 18:33:30 +0100

    belepesteszt13

--- includes/sqlconnect.php
+++ includes/sqlconnect.php
@@ -2,19 +2,23 @@
     try {
         // Kapcsolódás
 	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-	$mysqli = new mysqli("localhost", "webp1db", "J3grvN7YjfVtBGwD2RxzdS", "webp1db");
+	$dbh = new mysqli("localhost", "webp1db", "J3grvN7YjfVtBGwD2RxzdS", "webp1db");
 	if(mysqli_connect_errno()){
  				echo mysqli_connect_error();
 		}
-	$result = $mysqli->query("SELECT * FROM users", MYSQLI_USE_RESULT);
-	if($result){
-     // Cycle through results
-    while ($row = $result->fetch_object()){
-        $user_arr[] = $row;
-    }
-    // Free result set
-    $result->close();
-    //$db->next_result();
+	$dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
+	$result = $dbh->query("SELECT * FROM users", MYSQLI_USE_RESULT);
+	$sqlSelect = "select id, csaladinev, utonev from users where lname = zoli and passwd = sha512(X94T7Duxn6RFy3CY2ZqjQP)";
+	$sth = $dbh->query($sqlSelect);
+        //$sth->execute(array(':bejelentkezes' => $_POST['lname'], ':passwd' => $_POST['passwd']));
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+        if($row) {
+            $_SESSION['csn'] = $row['csaladinev']; $_SESSION['un'] = $row['utonev']; $_SESSION['login'] = $_POST['lname'];
+
+	}
+	echo "";
+        $result->close();
+        echo $_SESSION['csn'] = $row['csaladinev']; $_SESSION['un'] = $row['utonev']; $_SESSION['login'] = $_POST['lname'];
 }
     }
     catch (PDOException $e) {

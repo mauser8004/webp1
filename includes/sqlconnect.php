9e5545f180649ac880cb04b3a439e6ea1f710cf8
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-25 18:11:01 +0100

    belepesteszt3

---
+++ includes/sqlconnect.php
@@ -0,0 +1,23 @@
+<?php
+    try {
+        // Kapcsolódás
+        $dbh = new PDO('mysql:host=localhost;dbname=webp1db', 'webp1db', 'J3grvN7YjfVtBGwD2RxzdS',
+                        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
+        
+        // Felhsználó keresése
+        $sqlSelect = "select id, csaladinev, utonev from users where lname = :bejelentkezes and passwd = sha512(:passwd)";
+        $sth = $dbh->prepare($sqlSelect);
+        $sth->execute(array(':bejelentkezes' => $_POST['lname'], ':passwd' => $_POST['passwd']));
+        $row = $sth->fetch(PDO::FETCH_ASSOC);
+        if($row) {
+            $_SESSION['csn'] = $row['csaladinev']; $_SESSION['un'] = $row['utonev']; $_SESSION['login'] = $_POST['lname'];
+
+	}
+	echo "Sikerült";
+    }
+    catch (PDOException $e) {
+        $errormessage = "Hiba: ".$e->getMessage();
+    }      
+
+?>

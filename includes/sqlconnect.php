16cddb390bdc97f32b2f32b6d8358861260b72b3
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-25 18:38:17 +0100

    belepesteszt15

--- includes/sqlconnect.php
+++ includes/sqlconnect.php
@@ -7,18 +7,15 @@
  				echo mysqli_connect_error();
 		}
 	$dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
-	$result = $dbh->query("SELECT * FROM users", MYSQLI_USE_RESULT);
-	$sqlSelect = "select id, csaladinev, utonev from users where lname = zoli and passwd = sha512(X94T7Duxn6RFy3CY2ZqjQP)";
-	$sth = $dbh->query($sqlSelect);
-        //$sth->execute(array(':bejelentkezes' => $_POST['lname'], ':passwd' => $_POST['passwd']));
+	        // Felhsználó keresése
+        $sqlSelect = "select id, csaladinev, utonev from users where lname = :bejelentkezes and passwd = sha512(:passwd)";
+        $sth = $dbh->prepare($sqlSelect);
+        $sth->execute(array(':bejelentkezes' => 'zoli', ':passwd' => 'X94T7Duxn6RFy3CY2ZqjQP'));
         $row = $sth->fetch(PDO::FETCH_ASSOC);
         if($row) {
             $_SESSION['csn'] = $row['csaladinev']; $_SESSION['un'] = $row['utonev']; $_SESSION['login'] = $_POST['lname'];
 
 	}
-	echo "";
-        $result->close();
-        echo $_SESSION['csn'] = $row['csaladinev']; $_SESSION['un'] = $row['utonev']; $_SESSION['login'] = $_POST['lname'];
     }
     catch (PDOException $e) {
         $errormessage = "Hiba: ".$e->getMessage();

15f1d021baf2da115c891422a2e726aef63e38b4
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-26 05:25:06 +0100

    kilepesteszt3

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -17,8 +17,7 @@ $oldalak = array(
 	'kepek' => array('fajl' => 'gallery', 'szoveg' => 'Képek', 'menun' => array(1,1)),
 	'kapcsolat' => array('fajl' => 'conntact', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
     'uzenetek' => array('fajl' => 'messages', 'szoveg' => 'Üzenetek', 'menun' => array(1,1)),
-    'login' => array('fajl' => 'login', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'login' => array('fajl' => 'login', 'szoveg' => 'Belépés/Regisztráció', 'menun' => array(1,0)),
     'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
-    'regisztral' => array('login' => 'regisztral', 'szoveg' => 'Regisztrál', 'menun' => array(0,0))
 );
 ?>

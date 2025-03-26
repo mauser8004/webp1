71a4a043c1dc5c6047c6a38444caf536bae27abf
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-26 04:58:06 +0100

    belepesteszt26

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -17,9 +17,8 @@ $oldalak = array(
 	'kepek' => array('fajl' => 'gallery', 'szoveg' => 'Képek', 'menun' => array(1,1)),
 	'kapcsolat' => array('fajl' => 'conntact', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
     'uzenetek' => array('fajl' => 'messages', 'szoveg' => 'Üzenetek', 'menun' => array(1,1)),
-    'login' => array('fajl' => 'login', 'szoveg' => 'Belépés', 'menun' => array(1,1)),
+    'login' => array('fajl' => 'login', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
     'kilepes' => array('fajl' => 'login', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
-    'belep' => array('fajl' => 'login', 'szoveg' => '', 'menun' => array(0,0)),
-    'regisztral' => array('login' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+    'regisztral' => array('login' => 'regisztral', 'szoveg' => 'Regisztrál', 'menun' => array(0,0))
 );
 ?>

5c6be99c8edef19e6307817f0eb7b57ce3f31324
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-03-31 19:34:58 +0200

    regisztracio6jsteszt

--- templates/pages/login.tpl.php
+++ templates/pages/login.tpl.php
@@ -9,6 +9,7 @@
       </fieldset>
     </form>
     <h3>Regisztrálja magát, ha még nem felhasználó!</h2>
+    <form name="regForm" onsubmit="return validateForm(event)">
     <form action = "/includes/regisztral.php" method = "POST">
       <fieldset>
         <legend>Regisztráció</legend>
@@ -21,3 +22,44 @@
         <br>&nbsp;
       </fieldset>
     </form>
+</form>
+    <script>
+        function validateForm(event) {
+            event.preventDefault(); // Megakadályozza az űrlap elküldését, ha hibák vannak
+
+            let csaladinev = document.forms["regForm"]["csaladinev"].value;
+            let utonev = document.forms["regForm"]["utonev"].value;
+            let lname = document.forms["regForm"]["lname"].value;
+            let passwd = document.forms["regForm"]["passwd"].value;
+
+            let errorMsg = "";
+
+            // Vezetéknév és utónév ellenőrzése
+            let nameRegex = /^[A-ZÁÉÍÓÖŐÚÜŰ][a-záéíóöőúüű]{2,}$/;
+            if (!nameRegex.test(csaladinev)) {
+                errorMsg += "A vezetéknév nagybetűvel kell kezdődjön és legalább 3 karakter hosszú legyen!\n";
+            }
+            if (!nameRegex.test(utonev)) {
+                errorMsg += "Az utónév nagybetűvel kell kezdődjön és legalább 3 karakter hosszú legyen!\n";
+            }
+
+            // Felhasználónév ellenőrzése
+            if (lname.trim() === "") {
+                errorMsg += "A felhasználónév nem lehet üres!\n";
+            }
+
+            // Jelszó ellenőrzése
+            if (passwd.length < 8 || passwd.length > 20) {
+                errorMsg += "A jelszónak 8 és 20 karakter között kell lennie!\n";
+            }
+
+            // Hibaüzenetek megjelenítése
+            if (errorMsg !== "") {
+                alert(errorMsg);
+                return false;
+            }
+
+            alert("Sikeres regisztráció!");
+            document.forms["regForm"].submit(); // Ha minden jó, az űrlapot elküldi
+        }
+    </script>

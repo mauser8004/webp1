abacfbd6ff57fdc955c7730b0af07702f3e29c1b
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-04-01 05:13:32 +0200

    kepek3feltolthet

--- includes/belep.php
+++ includes/belep.php
@@ -22,11 +22,7 @@ if (isset($_POST['lname'], $_POST['passwd'])) {
         $row = $result->fetch_assoc();
         
         if ($row) {
-            //session_start();
-	    session_start([
-		    'cookie_lifetime' => 86400,
-    		    'read_and_close'  => true,
-		]);
+            session_start();
             $_SESSION['csn'] = $row['csaladinev'];
             $_SESSION['un'] = $row['utonev'];
             $_SESSION['login'] = $username;

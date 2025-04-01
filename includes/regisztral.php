1cacdf2eada148b0e5554d3f86ccddcceef7e012
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-04-01 03:12:27 +0200

    regisztracio22regteszt

--- includes/regisztral.php
+++ includes/regisztral.php
@@ -20,7 +20,7 @@ if(isset($_POST['csaladinev']) && isset($_POST['utonev']) && isset($_POST['lname
 	
 	
 	
-	$sqlSelect0 = "select id from users where lname = '$username'";
+	$sqlSelect0 = "select lname from users where lname = '$username'";
         $sth = $dbh->query($sqlSelect0);
 	if ($sth->num_rows = 0) {
 	

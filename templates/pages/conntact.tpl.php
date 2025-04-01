0f0e551f6818dc8a5e3bebcd8cdc5394fa6d7781
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-04-01 05:36:34 +0200

    kapcsolat1feltolthet

--- templates/pages/conntact.tpl.php
+++ templates/pages/conntact.tpl.php
@@ -1,1 +1,32 @@
-conntact
+<?php
+$comment =  "";
+
+if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
+<div>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+ <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+  <input type="submit" name="submit" value="Küldés">  
+</form>
+
+
+</div>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo "<br>";
+echo $comment;
+echo "<br>";
+?>

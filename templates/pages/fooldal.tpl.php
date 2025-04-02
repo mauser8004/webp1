8b25b7502752781a5cfd781326ad94b385170469
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-04-02 09:21:55 +0200

    fooldaltartalom4css

--- templates/pages/fooldal.tpl.php
+++ templates/pages/fooldal.tpl.php
@@ -8,7 +8,7 @@
         <img src="./images/arduino-board.jpg" alt="Arduino UNO tábla" class="float-right">
     </section>
            <div class="video-container">
-            <iframe width="560" height="315" src="https://youtu.be/gsuZPhkhH9g?si=KOkYaXom1-BCt6A9" title="YouTube video: Arduino bemutató" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ZLcKTGI_xHY?si=kzQ7SYlgeWYyLyP1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     <section class="advantages">
         <h2>Előnyök</h2>

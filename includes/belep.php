5e10b014280827ba2794a1c442c22a04c1f2f8ed
Zoltan Erdodi <zoltanerdodi@hotmail.com>
2025-04-19 07:13:33 +0200

    Probálom ugy ahogy a tankonyvben van beállitva a sql csatlakozás

--- includes/belep.php
+++ includes/belep.php
@@ -1,25 +1,40 @@
 <?php
+session_start();
 if (isset($_POST['lname'], $_POST['passwd'])) {
     try {
         // Database connection
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        $dbh = new mysqli("localhost", "webp1db", "J3grvN7YjfVtBGwD2RxzdS", "webp1db");
-        
-        if (mysqli_connect_errno()) {
+        /*mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+	$dbh = new mysqli("localhost", "webp1db", "J3grvN7YjfVtBGwD2RxzdS", "webp1db");*/
+
+	$dbh = new PDO('mysql:host=localhost;dbname=webp1db', 'webp1db', 'J3grvN7YjfVtBGwD2RxzdS',
+	array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+	$dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
+	$sqlSelect = "select csaladinev, utonev from users where lname = :bejelentkezes and passwd = :jelszo ";
+
+        $passwd = hash('sha512', $_POST['passwd']);
+	$sth = $dbh->prepare($sqlSelect);
+	// Az űrlapnál POST módszerrel küldjük el az adatokat.
+	$sth->execute(array(':bejelentkezes' => $_POST['lname'], ':jelszo' => $passwd ));
+	$row = $sth->fetch(PDO::FETCH_ASSOC);
+
+
+
+        /*if (mysqli_connect_errno()) {
             throw new mysqli_sql_exception(mysqli_connect_error());
         }
         
-        $dbh->set_charset("utf8");
+	$dbh->set_charset("utf8");*/
         
         // User authentication with prepared statement
-        $username = $dbh->real_escape_string($_POST['lname']); // Additional safety
+        /*$username = $dbh->real_escape_string($_POST['lname']); // Additional safety
         $passwd = hash('sha512', $_POST['passwd']);
         
         $stmt = $dbh->prepare("SELECT csaladinev, utonev FROM users WHERE lname = ? AND passwd = ?");
         $stmt->bind_param("ss", $username, $passwd);
         $stmt->execute();
         $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
+	$row = $result->fetch_assoc();*/
+
         
         if ($row) {
             session_start();
@@ -39,8 +54,8 @@ if (isset($_POST['lname'], $_POST['passwd'])) {
 	    } 
         
         
-        $stmt->close();
-        $dbh->close();
+        //$stmt->close();
+       // $dbh->close();
         
         header("Location: /");
         exit();
@@ -56,20 +71,3 @@ if (isset($_POST['lname'], $_POST['passwd'])) {
     exit();
 }
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Belépés</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <?php if(isset($uzenet)) { ?>
-            <h1><?= $uzenet ?></h1>
-            <?php if($ujra) { ?>
-                <a href="/login">Próbálja újra!</a>
-<?php
-		}
-
-         }  ?>
-    </body>
-</html>
